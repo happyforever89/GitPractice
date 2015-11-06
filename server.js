0d482302577a169af25b3a3aa30a430e3fb69aa1
happyforever89 <dongming.shu123@gmail.com>
2015-11-06 11:25:46 -0500

    practice

---
+++ server.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var server=http.createServer(function(req,res){
+	res.writeHead(200,{'Content-Type':'text/plain'});
+	res.end('Hello World,Dongming Shu\n');
+});
+console.log('Server running at http://1127.0.0.1:1337');
